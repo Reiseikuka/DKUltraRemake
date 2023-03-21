20b36a1d2b7c38c5441ad137d7ad1ab34966bbf3
Reiseikuka <reiseirakuka@outlook.com>
2023-03-21 02:00:16 -0600

    Hi score

--- Assets/ScoreScript.cs
+++ Assets/ScoreScript.cs
@@ -7,20 +7,36 @@ public class ScoreScript : MonoBehaviour
 {
 
     public Text currentscore;
+    public Text highscoreText;
+
+    private int highscore = 100;
+    private int playerscore;
+
     public BarrelDetection barrelscore;
-    void Start()
+
+    private void Start()
     {
-           currentscore = GetComponent<Text>();     
+           currentscore = GetComponent<Text>(); 
+
+           playerscore =  int.Parse(currentscore.text);
+           highscoreText.text = highscore.ToString();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         currentscore.text  = "" + barrelscore.score;
         /*For the moment, update the text 
           with the score gained by jumping barrels. Will be updated to 
           add whenever we destroy a barrel or pick items*/
         //GameScore = int.Parse(currentscore.text);
+        playerscore =  int.Parse(currentscore.text);
+
+        if (playerscore > highscore)
+        {
+            highscore = playerscore;
+            highscoreText.text  = currentscore.text;
+        }
     }
 
 }
